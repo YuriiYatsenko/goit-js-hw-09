761e84c4c83697faee00a548c1c9ae6834c6acc2
YuriiYatsenko <115614039+YuriiYatsenko@users.noreply.github.com>
2023-04-09 17:38:17 +0000

    Deploying to gh-pages from @ YuriiYatsenko/goit-js-hw-09@69598c710ab75f73ebe12e429bc8471539a51f98 🚀

---
+++ 01-color-switcher.2d5e645d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d),document.body.style.backgroundColor=""}));
+//# sourceMappingURL=01-color-switcher.2d5e645d.js.map
